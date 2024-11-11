5808e80045f75573fa50a830173fa1b8324d7ce4
GammaTT <52464999+GammaTT@users.noreply.github.com>
2024-11-11 20:14:42 +0900

    Initial commit

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed;
+
+    private Rigidbody rigidbody;
+
+    private Vector2 curMoveInput;
+
+
+    private void Awake()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+    }
+
+    private void Move()
+    {
+        transform.position += new Vector3(curMoveInput.x, 0, curMoveInput.y);
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        if (context.phase == InputActionPhase.Performed)
+        {
+            curMoveInput = context.ReadValue<Vector2>();
+        }
+        else if (context.phase == InputActionPhase.Canceled)
+        {
+            curMoveInput = Vector2.zero;
+        }
+
+        Move();
+    }
+}
